4f03db8298e99223ea27c5f814f1113607fdf2d5
João Santos <joaopns05@gmail.com>
2022-05-22 20:32:05 +0100

    add game skeleton

---
+++ flappy/src/main.rs
@@ -0,0 +1,22 @@
+struct GameState {}
+
+fn main() {
+    /*
+    let mut game_state = initial_game_state();
+    loop {
+        let input_state = capture_input_state();
+
+        input_system(&mut game_state, &mut input_state);
+
+        physics_system(&mut game_state);
+
+        // ...
+
+        render_system(&mut game);
+        audio_system(&mut game);
+
+        wait_vsync();
+    }
+     */
+
+}
