710f708863b0aa617bd3f7d8b5e8770d8d0851e2
João Santos <joaopns05@gmail.com>
2022-06-17 21:18:39 +0100

    add input logic

--- pong/src/main.rs
+++ pong/src/main.rs
@@ -3,6 +3,7 @@ use ggez::{
     graphics::{self, Color},
     mint::Point2,
     Context, ContextBuilder, GameError, conf,
+    input
 };
 
 use glam::*;
@@ -13,6 +14,16 @@ const PWIDTH_HALF: f32 = PLAYER_WIDTH / 2.0;
 const PHEIGHT_HALF: f32 = PLAYER_HEIGHT / 2.0;
 const BALL_SIZE: f32 = 10.0;
 const BSIZE_HALF: f32 = BALL_SIZE / 2.0;
+const PLAYER_SPEED: f32 = 600.0;
+
+
+fn clamp(val: &mut f32, min:f32, max: f32){
+    if *val > max {
+        *val = max;
+    } else if *val < min {
+        *val = min;
+    }
+}
 
 #[derive(Debug)]
 struct GameState {
@@ -35,16 +46,32 @@ impl GameState {
                 y: screen_height / 2.0,
             },
             ball_pos: Point2 {
-                x: (screen_width / 2.0) - BALL_SIZE,
-                y: (screen_height / 2.0) - BALL_SIZE,
+                x: screen_width / 2.0,
+                y: screen_height / 2.0,
             },
         };
         state
     }
+
+    fn handle_input(&mut self, ctx: &mut ggez::Context) {
+        let screen_height = ctx.gfx.drawable_size().1;
+        let dt = ctx.time.delta().as_secs_f32();
+        println!("{}", dt);
+
+        let ctx = &ctx.keyboard;
+        if ctx.is_key_pressed(input::keyboard::KeyCode::W) {
+            self.player1_pos.y -= PLAYER_SPEED * dt;
+        } else if ctx.is_key_pressed(input::keyboard::KeyCode::S) {
+            self.player1_pos.y += PLAYER_SPEED * dt;
+        }
+        clamp(&mut self.player1_pos.y, PHEIGHT_HALF, screen_height - PHEIGHT_HALF)
+    }
 }
 
 impl event::EventHandler for GameState {
-    fn update(&mut self, _ctx: &mut ggez::Context) -> Result<(), GameError> {
+    fn update(&mut self, ctx: &mut ggez::Context) -> Result<(), GameError> {
+        
+        GameState::handle_input(self, ctx);
         Ok(())
     }
 
@@ -80,10 +107,13 @@ impl event::EventHandler for GameState {
         let drawparams = graphics::DrawParam::new()
         .dest(self.ball_pos);
         canvas.draw(&ball_mesh, drawparams);
-
         canvas.finish(ctx)
+        
     }
 }
+
+
+
 fn main() {
     let (mut ctx, event_loop) = ContextBuilder::new("master pong", "jpns")
         .window_setup(conf::WindowSetup {
